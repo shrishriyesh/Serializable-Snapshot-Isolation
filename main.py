e706fca3b55af1854e007752bc4818680fe19894
Shriyesh <shriyesh.chandra@gmail.com>
2024-12-09 12:35:51 -0500

    Changes to Main so that it takes input from an inputs/ folder

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from transaction_manager import TransactionManager
+import os
 
-def main():
+def process_input(input_file, input_folder):
     tm = TransactionManager()
-    input_file = 'input.txt'
+    input_file = os.path.join(input_folder, input_file)
 
     with open(input_file, 'r') as file:
         for line in file:
@@ -12,6 +13,14 @@ def main():
                 continue
             print(f"> {line}")
             tm.process_command(line)
+
+def main():
+    input_folder = './inputs'
+    #iterate through all the input files in the input folder and call process input on the files
+    for file in os.listdir(input_folder):
+        print(f"Processing {file}")
+        process_input(file, input_folder)
+        print("--------------------------------------------------------------------------------------")
 
 if __name__ == '__main__':
     main()
